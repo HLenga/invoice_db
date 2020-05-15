5b97178bf8a2fdf8f49c7beb565612787bb10c05
Hlenga <anghongleng@gmail.com>
2020-05-15 18:23:12 +0800

    add navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/faq'
+  get 'welcome/pricing'
+  get 'welcome/features'
   resources :invoices
+  root to:'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
